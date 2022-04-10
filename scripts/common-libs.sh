0eeeefe9c2b52ec1509af86fe5d4d62e43a4c09d
Anthony Yeong <anthony.yeong@partior.com>
2022-04-10 16:59:40 +0800

    DSO-545 Add version storing feature

---
+++ scripts/common-libs.sh
@@ -0,0 +1,26 @@
+## Most of the function here require the variable names flatten from its yaml key name.
+## Example, in yaml, "sample=project.config.feature-enabled=false" structured as:
+##                  sample-project
+#                       config
+#                           feature-enabled: false
+##  The equivalent flatten key:
+##                  - sample_project__config_feature__enabled=false
+
+function getListCount() {
+    local searchQueryPath="$1"
+    local searchQueryPathConverted="$(convertQueryToEnv "$searchQueryPath")"
+    set | grep -e "^${searchQueryPathConverted}__" | wc -l
+}
+
+function convertQueryToEnv() {
+    local inputQueryPath=$1
+    local converted=$(echo $inputQueryPath | sed  "s/\./__/g" |  sed "s/\-/_/g" )
+    echo $converted
+}
+
+function getValueByQueryPath() {
+    local searchQueryPath=$1
+    local searchQueryPathConverted=$(convertQueryToEnv "$searchQueryPath")
+    local foundValue=$(set | grep -e "^${searchQueryPathConverted}=" | cut -d"=" -f1 --complement)
+    echo $foundValue
+}
