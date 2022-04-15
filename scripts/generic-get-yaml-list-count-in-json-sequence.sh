a3b2151d86d6ddc9d89314418b9dacdd024a9539
Anthony Yeong <anthony.yeong@partior.com>
2022-04-15 21:50:29 +0800

    DSO-32 Add list to sequence converter

---
+++ scripts/generic-get-yaml-list-count-in-json-sequence.sh
@@ -0,0 +1,44 @@
+#!/bin/bash +e
+set +e
+
+## Sourcing the common libs
+if [[ ! -z $BASH_SOURCE ]]; then
+    ACTION_BASE_DIR=$(dirname $BASH_SOURCE)
+    source $(find $ACTION_BASE_DIR/.. -type f | grep common-libs.sh)
+elif [[ $(find . -type f -name common-libs.sh | wc -l) > 0 ]]; then
+    source $(find . -type f | grep common-libs.sh)
+elif [[ $(find .. -type f -name common-libs.sh | wc -l) > 0 ]]; then
+    source $(find .. -type f | grep common-libs.sh)
+else
+    echo "[ERROR] $BASH_SOURCE (line:$LINENO): Unable to find and source common-libs.sh"
+    exit 1
+fi
+
+listQueryPath="$1"
+
+function convertListToJsonSequence() {
+    local searchQueryPath="$1"
+
+    local listCount=$(getListCount "$searchQueryPath")
+
+    if [[ $listCount -eq 0 ]]; then
+        echo "[INFO] Sequence list from [$searchQueryPath] is 0. Skipping"
+        exit 0
+    fi
+
+    local tmpBuffer=""
+    for eachSequenceItem in `seq 0 $((${listCount}-1))`
+    do    
+        if [[ -z "$tmpBuffer" ]]; then
+            tmpBuffer="[ '$eachSequenceItem'"
+        else
+            tmpBuffer="$tmpBuffer, '$eachSequenceItem'"
+        fi
+    done
+    tmpBuffer="$tmpBuffer ]"
+    echo $tmpBuffer
+}
+
+finalJsonList=$(convertListToJsonSequence "$listQueryPath")
+echo "[INFO] Found list value: $finalJsonList"
+echo ::set-output name=list::"$finalJsonList"
