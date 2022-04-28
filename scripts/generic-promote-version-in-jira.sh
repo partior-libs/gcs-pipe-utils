b056b164ff1baeff5c75ccfe58b855f0421eefbc
ArshPartior <sayeda.arshia@partior.com>
2022-04-28 11:29:57 +0530

    DSO-594-Jira artifact promotion

--- scripts/generic-promote-version-in-jira.sh
+++ scripts/generic-promote-version-in-jira.sh
@@ -1,19 +1,6 @@
 #!/bin/bash +e
 set +e
 
-## Sourcing the common libs
-if [[ ! -z $BASH_SOURCE ]]; then
-    ACTION_BASE_DIR=$(dirname $BASH_SOURCE)
-    source $(find $ACTION_BASE_DIR/.. -type f | grep common-libs.sh)
-elif [[ $(find . -type f -name common-libs.sh | wc -l) > 0 ]]; then
-    source $(find . -type f | grep common-libs.sh)
-elif [[ $(find .. -type f -name common-libs.sh | wc -l) > 0 ]]; then
-    source $(find .. -type f | grep common-libs.sh)
-else
-    echo "[ERROR] $BASH_SOURCE (line:$LINENO): Unable to find and source common-libs.sh"
-    exit 1
-fi
-
 jiraUsername="$1"
 jiraToken="$2"
 jiraBaseUrl="$3"
