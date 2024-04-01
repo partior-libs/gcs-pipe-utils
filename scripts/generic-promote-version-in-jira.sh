1189cc9ade5e9018f5b588ec1148be9a0a7f6e66
Anthony Yeong <anthony.yeong@partior.com>
2024-04-01 03:50:34 +0000

    DSO-2862: Add new workflow to get Github member details

--- scripts/generic-promote-version-in-jira.sh
+++ scripts/generic-promote-version-in-jira.sh
@@ -22,7 +22,7 @@ function getSourceVersionId() {
     response=$(curl -k -s -u $jiraUsername:$jiraToken \
                 -w "status_code:[%{http_code}]" \
                 -X GET \
-                "$jiraBaseUrl/rest/api/3/project/$jiraProjectKey/versions" -o $responseOutFile)
+                "$jiraBaseUrl/rest/api/3/project/$jiraProjectKey/versions")
 
     if [[ $? -ne 0 ]]; then
         echo "[ACTION_CURL_ERROR] $BASH_SOURCE (line:$LINENO): Error running curl to get the project version details."
