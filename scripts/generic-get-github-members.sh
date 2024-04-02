36468cd2344bf1f0ec199a7d8e4e5cc9d2002687
Anthony Yeong - Partior <97429688+anthony-yeong-partior@users.noreply.github.com>
2024-04-02 10:39:52 +0800

    DSO-2862: Add new workflow to get Github member details (#3)
    * DSO-2862: Add new workflow to get Github member details
    
    * DSO-2862: Update file permission
    
    * DSO-2862: Introduce delay and finetune
    
    * DSO-2862: Fix json output
    
    * DSO-2862: Restored a file
    
    * DSO-2862: Commented out debug line
    
    * DSO-2862: Pick the lib branch automatically
    
    * DSO-2862: Pick the lib branch automatically
    
    * DSO-2862: Fix lib branch
    
    * DSO-2862: Fix lib branch
    
    * DSO-2862: Remove debug line

---
+++ scripts/generic-get-github-members.sh
@@ -0,0 +1,72 @@
+set +e
+
+## Sourcing the common libs
+if [[ ! -z $BASH_SOURCE ]]; then
+    ACTION_BASE_DIR=$(dirname $BASH_SOURCE)
+    source $(find $ACTION_BASE_DIR/.. -type f | grep common-libs.sh)
+elif [[ $(find . -type f -name common-libs.sh | wc -l) > 0 ]]; then
+    source $(find . -type f | grep common-libs.sh)
+elif [[ $(find .. -type f -name common-libs.sh | wc -l) > 0 ]]; then
+    source $(find .. -type f | grep common-libs.sh)
+else
+    echo "[ERROR] $BASH_SOURCE (line:$LINENO): Unable to find and source common-libs.sh"
+    exit 1
+fi
+
+
+targetSearchUser="$1"
+targetJsonFile="$2"
+targetCsvFile="$3"
+targetUsernameValueFile="${4:-user.tmp}"
+targetUserEmailValueFile="${5:-email.tmp}"
+
+## For this script to work, there must be valid github auth token and cookie session in the following env
+## GH_AUTH_TOKEN
+## GH_AUTH_COOKIE_SESSION
+authToken="${GH_AUTH_TOKEN}"
+cookieSession="${GH_AUTH_COOKIE_SESSION}"
+
+#echo "[DEBUG] authToken: $authToken"
+#echo "[DEBUG] cookieSession: $cookieSession"
+
+downloadLink=$(ghGetMembersCsvDownloadLink "$authToken" "$cookieSession")
+if [[ $? -gt 0 ]]; then
+    echo "[ERROR] $BASH_SOURCE (line:$LINENO): Unable to mget dowload link:"
+    echo "[ERROR_MSG] $downloadLink"
+    exit 1
+fi
+
+echo "[INFO] Retrieved download link: $downloadLink"
+echo "[INFO] Sleep 2sec..."
+sleep 2
+echo "[INFO] Start Downloading CSV data..."
+echo ghGetMembersCsvFile "$cookieSession" "$downloadLink" "$targetCsvFile"
+ghGetMembersCsvFile "$cookieSession" "$downloadLink" "$targetCsvFile"
+if [[ $? -gt 0 ]]; then
+    echo "[ERROR] $BASH_SOURCE (line:$LINENO): Unable to dowload CSV member list"
+    echo "[ERROR_MSG] $(cat $targetCsvFile)"
+    exit 1
+fi
+
+echo "[INFO] Convert to Json..."
+convertGhCsvToJson "$targetCsvFile" "$targetJsonFile"
+if [[ $? -gt 0 ]]; then
+    echo "[ERROR] $BASH_SOURCE (line:$LINENO): Unable to dowload CSV member list"
+    echo "[ERROR_MSG] $(cat $targetCsvFile)"
+    exit 1
+fi
+
+echo "[INFO] Preview Json..."
+cat $targetJsonFile | jq . 
+
+echo "[INFO] Lookup for user [$targetSearchUser]..."
+foundUser=$(findGhUser "$targetSearchUser" "$targetJsonFile" "$targetUsernameValueFile" "$targetUserEmailValueFile")
+if [[ $? -gt 0 ]]; then
+    echo "[ERROR] $BASH_SOURCE (line:$LINENO): Unable to lookup user [$targetSearchUser]"
+    echo "[ERROR_MSG] $(echo $foundUser)"
+    exit 1
+fi
+
+echo "[INFO] Found user lookup: [$foundUser]..."
+
+exit 0
