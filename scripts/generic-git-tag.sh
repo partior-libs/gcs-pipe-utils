3f7b2a87da8e66caddc37beda92c1d1d24c39fa5
Anthony Yeong <anthony.yeong@partior.com>
2025-01-15 18:15:58 +0000

    DSO-3258: Use API to delete tag

--- scripts/generic-git-tag.sh
+++ scripts/generic-git-tag.sh
@@ -16,8 +16,12 @@ function startTag() {
     git config --local user.name github-actions
     git config --local user.email github-actions@github.com
     #git remote set-url origin https://${githubPatToken}@github.com/${targetRepo}
-    echo "[INFO] Deleting remotee tag if existed..."
-    git push --delete origin $finalVersion || true
+    echo "[INFO] Deleting remote tag if existed..."
+    curl -X DELETE \
+        -H "Accept: application/vnd.github.v3+json" \
+        -H "Authorization: token ${githubPatToken}" \
+        https://api.github.com/repos/${targetRepo}/git/refs/tags/$finalVersion
+    #git push --delete origin $finalVersion || true
     echo "[INFO] Push tag..."
     git push --tags
 }
